2583216ed644341ba8c1dcd4b36171e91c78cd6f
sanjjey <63184889+sanjjey@users.noreply.github.com>
2025-05-09 13:11:52 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
     hardhat: {
     },
     sepolia: {
-      url: "https://sepolia.infura.io/v3/58526e4ef7ab49548360d15b63de81b8",
-      accounts: ["0x02726821a7eb58705bc919ea325d80294a3f2ea5"]
+      url: "https://sepolia.infura.io/v3/<key>",
+      accounts: ["your_private_key"]
     }
   },
   solidity: {
